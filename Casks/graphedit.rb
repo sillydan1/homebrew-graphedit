31d385e80d590716fda1ae98c52fc786527ebda0
Asger Gitz-Johansen <sillydan1@users.noreply.github.com>
2023-10-06 17:25:27 +0200

    Update graphedit.rb

--- Casks/graphedit.rb
+++ Casks/graphedit.rb
@@ -7,7 +7,7 @@ cask "graphedit" do
 
   homepage "https://github.com/sillydan1/graphedit"
 
-  url "https://github.com/sillydan1/graphedit/releases/download/v1.0.0/Graphedit-1.0.0.dmg"
+  url "https://github.com/sillydan1/graphedit/releases/download/v1.0.0/graphedit.dmg"
 
   sha256 "e292e545809ae56c5ad12318e0d5fc10c98149bf5f05d717cbdc2fb77fd1b770"
 
